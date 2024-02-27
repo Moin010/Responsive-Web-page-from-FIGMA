738a32f3134fa09c7bb66e7bb2ae9ade2fc87b09
Moin Alam <52396153+Moin010@users.noreply.github.com>
2024-02-27 18:55:20 +0600

    trimed css file and added instruction for js file

--- js/main.js
+++ js/main.js
@@ -5,3 +5,4 @@
 //         nav.classlist.add("scrolled");
 //     }
 // })
+// add " Scrolled" in the "nav-desktop" at header>wrapper>nav-desktop &&& "scrolled-img" in the "img" tag at header>wrapper>hero-section>right-side>img with Js to make the nav sticky
