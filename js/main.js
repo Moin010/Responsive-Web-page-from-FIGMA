00914455250a986a1c8dc8949b0865b37cfc584d
Moin Alam <moinalam010@gmail.com>
2024-02-28 18:12:03 +0600

    added sitcky menu for desktop and tablet

--- js/main.js
+++ js/main.js
@@ -5,17 +5,28 @@
 //         nav.classlist.add("scrolled");
 //     }
 // })
-// add " Scrolled" in the "nav-desktop" at header>wrapper>nav-desktop &&& "scrolled-img" in the "img" tag at header>wrapper>hero-section>right-side>img with Js to make the nav sticky
+// add " scrolled" in the "nav-desktop" at header>wrapper>nav-desktop &&& "scrolled-img" in the "img" tag at header>wrapper>hero-section>right-side>img with Js to make the nav sticky
+
+const navbar = document.querySelector(".nav-desktop");
+const scrollOnYAxis = navbar.scrollHeight;
+
+window.onscroll = () => {
+	if (window.scrollY >= scrollOnYAxis) {
+		navbar.classList.add("scrolled");
+	} else {
+		navbar.classList.remove("scrolled");
+	}
+};
 // mobile menu active class add for click event
 const mobileMenu = document.querySelector(".menu-m");
 const hamburgerMenuBtn = document.querySelector(".hamburger-icon");
 const crossbutton = document.querySelector("#cross-btn");
 
-hamburgerMenuBtn.addEventListener("click", function () {
+hamburgerMenuBtn.addEventListener("click", () => {
 	console.log("hamburger menu clicked");
 	mobileMenu.classList.add("mobile-active");
 });
-crossbutton.addEventListener("click", function () {
+crossbutton.addEventListener("click", () => {
 	console.log("hamburger menu clicked");
 	mobileMenu.classList.remove("mobile-active");
 });
