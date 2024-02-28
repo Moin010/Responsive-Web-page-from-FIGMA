82f67df307d578d8f62a5d6e8f7cf399b920c8d9
Moin Alam <moinalam010@gmail.com>
2024-02-28 17:02:16 +0600

    added mobile menu

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@
 // })
 // add " Scrolled" in the "nav-desktop" at header>wrapper>nav-desktop &&& "scrolled-img" in the "img" tag at header>wrapper>hero-section>right-side>img with Js to make the nav sticky
 // mobile menu active class add for click event
+const mobileMenu = document.querySelector(".menu-m");
+const hamburgerMenuBtn = document.querySelector(".hamburger-icon");
+const crossbutton = document.querySelector("#cross-btn");
+
+hamburgerMenuBtn.addEventListener("click", function () {
+	console.log("hamburger menu clicked");
+	mobileMenu.classList.add("mobile-active");
+});
+crossbutton.addEventListener("click", function () {
+	console.log("hamburger menu clicked");
+	mobileMenu.classList.remove("mobile-active");
+});
